b17dfb1a39de1231876037bd0c777286066ff614
MonsterDruide1 <5958456@gmail.com>
2022-08-09 17:54:06 +0200

    gfx: Add Viewport header

---
+++ include/gfx/seadViewport.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <gfx/seadProjection.h>
+#include <math/seadBoundBox.h>
+#include <math/seadVector.h>
+
+namespace sead
+{
+template <typename T>
+class Ray;
+class DrawContext;
+class LogicalFrameBuffer;
+class Projection;
+class Camera;
+
+class Viewport : public BoundBox2f
+{
+public:
+    Viewport();
+    Viewport(float left, float top, float right, float bottom);
+    explicit Viewport(const BoundBox2f&);
+    explicit Viewport(const LogicalFrameBuffer&);
+    virtual ~Viewport();
+
+    void setByFrameBuffer(const LogicalFrameBuffer&);
+    void apply(DrawContext*, const LogicalFrameBuffer&) const;
+    void getOnFrameBufferPos(Vector2f*, const LogicalFrameBuffer&) const;
+    void getOnFrameBufferSize(Vector2f*, const LogicalFrameBuffer&) const;
+    void applyViewport(DrawContext*, const LogicalFrameBuffer&) const;
+    void applyScissor(DrawContext*, const LogicalFrameBuffer&) const;
+    void project(Vector2f*, const Vector3f&) const;
+    void project(Vector2f*, const Vector2f&) const;
+    void unproject(Vector3f*, const Vector2f&, const Projection&, const Camera&) const;
+    void unproject(Ray<Vector3f>*, const Vector2f&, const Projection&, const Camera&) const;
+
+private:
+    Graphics::DevicePosture mDevicePosture;
+};
+}  // namespace sead
