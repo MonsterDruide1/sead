5488dd2b54ebb8cfec7bbc7d02176ca62fe926f2
MonsterDruide1 <5958456@gmail.com>
2022-08-16 10:53:16 +0200

    unfinished

--- include/controller/nin/seadNinJoyNpadDevice.h
+++ include/controller/nin/seadNinJoyNpadDevice.h
@@ -3,6 +3,14 @@
 #include "controller/seadControlDevice.h"
 #include "controller/seadController.h"
 #include "heap/seadHeap.h"
+#include "thread/seadCriticalSection.h"
+#include "thread/seadThread.h"
+
+namespace nn::hid {
+    struct VibrationDeviceHandle;
+    struct VibrationValue;
+    enum NpadJoyHoldType {};
+}
 
 namespace sead
 {
@@ -10,12 +18,31 @@ namespace sead
 class NinJoyNpadDevice : public ControlDevice
 {
 public:
+    class VibrationThread : public Thread {
+    public:
+        VibrationThread(Heap* heap);
+        ~VibrationThread() override;
+
+        void requestVibration(const nn::hid::VibrationDeviceHandle& deviceHandle, const nn::hid::VibrationValue& value);
+
+    protected:
+        void calc_(MessageQueue::Element msg) override;
+
+    private:
+        void* gaps[43];
+        CriticalSection mCS;
+    };
+
+
     NinJoyNpadDevice(ControllerMgr* mgr, Heap* heap);
 
     void calc() override;
 
 private:
-    u8 filler[0x8620];
+    s32 mNpadIdUpdateNum;
+    nn::hid::NpadJoyHoldType mNpadJoyHoldType;
+    void* filler[4208];
+    VibrationThread mVibrationThread;
 };
 
 }  // namespace sead
