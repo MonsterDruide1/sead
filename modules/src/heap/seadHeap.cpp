db5f57f24bb9eeab2333d2ae382455d86109e7c6
MonsterDruide1 <5958456@gmail.com>
2024-06-07 01:26:51 +0200

    Prepare `sead::Heap`s for memory management
    Making heavy use of https://github.com/stupidestmodder/newergfx

--- modules/src/heap/seadHeap.cpp
+++ modules/src/heap/seadHeap.cpp
@@ -4,6 +4,43 @@
 
 namespace sead
 {
+Heap::Heap(const SafeString& name, Heap* parent, void* start, size_t size, HeapDirection direction, bool enableLock)
+    : IDisposer(parent, HeapNullOption::UseSpecifiedOrContainHeap)
+    , INamable(name)
+    , mStart(start)
+    , mSize(size)
+    , mParent(parent)
+    , mChildren()
+    , mListNode()
+    , mDisposerList()
+    , mDirection(direction)
+    , mCS(parent)
+#ifdef SEAD_DEBUG
+    , mFlag((1 << Flag::eEnableWarning) | (1 << Flag::eEnableDebugFillSystem) | (1 << Flag::eEnableDebugFillUser))
+#else
+    , mFlag(1 << Flag::cEnableWarning)
+#endif // SEAD_DEBUG
+    //, mHeapCheckTag(static_cast<u16>(HeapMgr::getHeapCheckTag()))
+#ifdef SEAD_DEBUG
+    , mAccessThread(nullptr)
+#endif // SEAD_DEBUG
+{
+    mFlag.changeBit(Flag::cEnableLock, enableLock);
+
+    ConditionalScopedLock<CriticalSection> lock(&mCS, isEnableLock());
+
+    mChildren.initOffset(offsetof(Heap, mListNode));
+    mDisposerList.initOffset(IDisposer::getListNodeOffset());
+}
+
+void Heap::pushBackChild_(Heap* child)
+{
+    ScopedLock<CriticalSection> treeLock(HeapMgr::getHeapTreeLockCS_());
+    ConditionalScopedLock<CriticalSection> heapLock(&mCS, isEnableLock());
+
+    mChildren.pushBack(child);
+}
+
 Heap::~Heap() = default;
 
 void Heap::appendDisposer_(IDisposer* disposer)
